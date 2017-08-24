ea44aeab681526d026423e5feee3d0bcc04048dc
Jarrett Revels <jarrettrevels@gmail.com>
2017-08-23 20:44:54 -0400

    fix diffrule usage

--- src/ForwardDiff.jl
+++ src/ForwardDiff.jl
@@ -10,28 +10,28 @@ using DiffRules
 
 for (M, f, arity) in PRIMITIVES
     if arity == 1
-        dfdx = diffrule(M, f, arity)
+        dfdx = diffrule(M, f, :vx)
         @eval begin
             @primitive DualCtx function @ctx(c::typeof($f))(@ctx(x))
                 vx, dx = value(c, x), meta(c, x)
-                return Dual(c, $f(vx), propagate($dfdx(vx), dx))
+                return Dual(c, $f(vx), propagate($dfdx, dx))
             end
         end
     elseif arity == 2
-        dfdx, dfdy = diffrule(M, f, arity)
+        dfdx, dfdy = diffrule(M, f, :vx, :vy)
         @eval begin
             @primitive DualCtx function @ctx(c::typeof($f))(@ctx(x), @ctx(y))
                 vx, dx = value(c, x), meta(c, x)
                 vy, dy = value(c, y), meta(c, y)
-                return Dual(c, $f(vx, vy), propagate($dfdx(vx, vy), dx, $dfdy(vx, vy), dy))
+                return Dual(c, $f(vx, vy), propagate($dfdx, dx, $dfdy, dy))
             end
-            @primitive DualCtx function @ctx(c::typeof($f))(@ctx(x), y)
+            @primitive DualCtx function @ctx(c::typeof($f))(@ctx(x), vy)
                 vx, dx = value(c, x), meta(c, x)
-                return Dual(c, $f(vx, y), propagate($dfdx(vx, y), dx))
+                return Dual(c, $f(vx, y), propagate($dfdx, dx))
             end
-            @primitive DualCtx function @ctx(c::typeof($f))(x, @ctx(y))
+            @primitive DualCtx function @ctx(c::typeof($f))(vx, @ctx(y))
                 vy, dy = value(c, x), meta(c, x)
-                return Dual(c, $f(x, vy), propagate($dfdy(x, vy), dy))
+                return Dual(c, $f(x, vy), propagate($dfdy, dy))
             end
         end
     end
